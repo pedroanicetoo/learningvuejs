fa12b896df847c18355979cc5a9335972390649e
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2021-02-06 18:48:13 -0300

    [TECHNO-PROJECT] modal behaviour by vuejs functions

--- techno-project/app.js
+++ techno-project/app.js
@@ -25,11 +25,20 @@ const vm = new Vue({
         .then(r => r.json())
         .then(r => {
           this.produto = r;
-        })
+      })
+    },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+    fecharModal({ target, currentTarget }) {
+      if(target === currentTarget) this.produto = false;
     }
   },
   created() {
-    console.log('oi');
     this.fetchProdutos();
   }
 }) 
