83ad69b3d4fa04878334e17fe628b54ab96a9fab
pedroanicetoo <pedro_madri@hotmail.com>
2021-02-14 03:32:27 -0300

    [SLOTS] start default project and basic example of slots properties

---
+++ vue-slots/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <modal-principal>
+      <template v-slot:header="{ logo }">
+        {{logo}}
+        <p>Esse é o header</p>
+      </template>
+      <template v-slot:default="{ user }">
+        {{user.nome}}
+        <p>Esse é um teste</p>
+        <button>Comprar</button>
+      </template>
+      <template v-slot:footer>
+        <p>Esse é o footer</p>
+      </template>
+    </modal-principal>
+  </div>
+</template>
+
+<script>
+import ModalPrincipal from "./components/ModalPrincipal.vue";
+export default {
+  name: "App",
+  components: {
+    ModalPrincipal
+  }
+};
+</script>
+
+<style>
+</style>
