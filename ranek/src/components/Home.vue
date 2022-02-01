3cbc165ed3824b492bf6082cdafe8ff429cf4d4f
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2022-02-01 14:42:41 -0300

    start ranek project

---
+++ ranek/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    AAAAAAAAAAAAAAAAAAAAAAAAAAA
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  props: {
+    msg: String
+  }
+}
+</script>
