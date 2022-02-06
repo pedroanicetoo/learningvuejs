9fd470f49e22fd1bb0bd7f8b63e15338b7aeaa52
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2022-02-06 02:23:34 -0300

    Add axios

--- ranek/src/components/ProdutosLista.vue
+++ ranek/src/components/ProdutosLista.vue
@@ -10,6 +10,9 @@
 </template>
 
 <script>
+
+import { api } from '@/services.js';
+
 export default {
   data() {
     return {
@@ -18,10 +21,9 @@ export default {
   },
   methods: {
     getProdutos() {
-      fetch("http://localhost:3000/produto").then(response => response.json()).then
-      (response => {
-        this.produtos = response;
-      })
+      api.get("/produto").then(response => {
+        this.produtos = response.data;
+      });
     }
   },
   created() {
