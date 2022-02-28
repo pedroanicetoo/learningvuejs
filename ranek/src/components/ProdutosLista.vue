cb52161cd15aa92ab0db10d9d3d8c4bbf8d347bd
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2022-02-28 14:55:01 -0300

    pagination complete

--- ranek/src/components/ProdutosLista.vue
+++ ranek/src/components/ProdutosLista.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="produtos-container">
     <div v-if="produtos && produtos.length" class="produtos">
-      <div class="produto" v-for="produto in produtos" :key="produto.id">
+      <div class="produto" v-for="(produto, index) in produtos" :key="index">
         <router-link to="/">
           <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
           <p class="preco">{{produto.preco}}</p>
@@ -9,6 +9,7 @@
           <p>{{produto.descricao}}</p>
         </router-link>
       </div>
+      <ProdutosPaginar :produtosTotal="produtosTotal" :produtosPorPagina="produtosPorPagina"/>
     </div>
     <div v-else-if="produtos && produtos.length === 0">
       <p class="sem-resultados">Busca sem resultado, tente outro termo.</p>
@@ -18,14 +19,20 @@
 
 <script>
 
+import ProdutosPaginar from '@/components/ProdutosPaginar.vue';
 import { api } from '@/services.js';
 import { serialize } from '@/helpers.js';
 
 export default {
+  name: "ProdutosLista",
+  components: {
+    ProdutosPaginar
+  },
   data() {
     return {
       produtos: null,
-      produtosPorPagina: 9,
+      produtosPorPagina: 3,
+      produtosTotal: 0
     }
   },
   computed: {
@@ -42,6 +49,8 @@ export default {
   methods: {
     getProdutos() {
       api.get(this.url).then(response => {
+        this.produtosTotal = Number(response.headers["x-total-count"]);
+        console.log(response)
         this.produtos = response.data;
       });
     }
