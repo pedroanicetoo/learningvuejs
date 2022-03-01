2bbf417defe4d19f06c5af1427c7cc9e7b523ff2
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2022-03-01 01:47:56 -0300

    CEP api

--- ranek/src/services.js
+++ ranek/src/services.js
@@ -7,5 +7,12 @@ const axiosInstance = axios.create({
 export const api = {
   get(endpoint) {
     return axiosInstance.get(endpoint);
+  },
+  post(endpoint, body) {
+    return axiosInstance.post(endpoint, body);
   }
 };
+
+export function getCep(cep) {
+  return axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+}
